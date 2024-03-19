ea2f151e7ed7dadce259ed7ff56d1b2de8c564cb
Kong-E <kng001016@dgu.ac.kr>
2024-03-19 14:22:15 +0900

    Add: PostDetailModal

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,5 +1,18 @@
 import { Post } from '@/components/post/Post'
+import { POST_DUMMY_DATA } from '@/utils/Dummy'
 
 export default function PostPage() {
-  return <Post />
+  return (
+    <div className="flex flex-col min-h-screen mb-16">
+      <main className="flex-1">
+        <div className="container px-4 pb-6 pt-3 mx-auto space-y-6">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {POST_DUMMY_DATA.map((data) => (
+              <Post key={data.photo_id} data={data} />
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
---
+++ src/types/post.ts
@@ -0,0 +1,12 @@
+export interface PostDetailType {
+  photo_id: string
+  photo_title: string
+  photo_description: string
+  photo_url: string
+  photo_created_at: string
+  photographer_id: string
+  photographer_nickname: string
+  photographer_profile_img: string
+  photo_abusing_report_count: number | null
+  photographer_photo_abusing_report_count: number | null
+}
