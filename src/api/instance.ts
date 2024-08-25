a58b64b0540a96a390281619c97ab0999e9b4d01
Kong-E <kng001016@dgu.ac.kr>
2024-08-25 18:34:27 +0900

    Add: instance에 try-catch 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -10,35 +10,38 @@ const getAuthToken = () => {
 }
 
 const fetchWithAuth = async (url: string, options: RequestInit = {}) => {
-  const token = getAuthToken()
+  try {
+    const token = getAuthToken()
 
-  if (token) {
-    options.headers = {
-      ...options.headers,
-      Authorization: `Bearer ${token}`,
+    if (token) {
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${token}`,
+      }
     }
-  }
 
-  const response = await fetch(`${BASE_URL}${url}`, options)
+    const response = await fetch(`${BASE_URL}${url}`, options)
 
-  if (!response.ok) {
-    console.error('Network response was not ok', response)
-    throw new Error(`HTTP error! status: ${response.status}`)
-  }
-
-  // 응답 본문의 내용 타입 확인
-  const contentType = response.headers.get('content-type')
-  if (contentType && contentType.indexOf('application/json') !== -1) {
-    try {
-      return await response.json()
-    } catch (error) {
-      console.error('Error parsing JSON:', error)
-      throw new Error('Invalid JSON response')
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-  } else {
-    // JSON이 아닌 경우, 텍스트로 반환
-    return await response.text()
+
+    // 응답 본문의 내용 타입 확인
+    const contentType = response.headers.get('content-type')
+    if (contentType && contentType.indexOf('application/json') !== -1) {
+      try {
+        return await response.json()
+      } catch (error) {
+        console.error('Error parsing JSON:', error)
+        throw new Error('Invalid JSON response')
+      }
+    } else {
+      // JSON이 아닌 경우, 텍스트로 반환
+      return await response.text()
+    }
+  } catch (error) {
+    console.error('Error fetching data:', (error as Error).message)
   }
 }
 
-export const instance = fetchWithAuth
+export const instance = fetchWithAuth;
--- src/api/photo.ts
+++ src/api/photo.ts
@@ -5,13 +5,9 @@ import { instance } from './instance'
 
 // 사진 페이징 조회
 export const getPhotos = async () => {
-  try {
-    const response = await instance('/photo')
-    return response // 200 상태 코드는 fetchWithAuth에서 처리됨
-  } catch (error) {
-    console.error('Error getting photos:', (error as Error).message)
-    throw error
-  }
+  const response = await instance('/photo')
+
+  return response // 200 상태 코드는 fetchWithAuth에서 처리됨
 }
 
 // 사진 상세 조회
@@ -31,15 +27,9 @@ export const deletePhoto = async (photo_id: string) => {
 
 // 사진 삭제 요청 반려
 export const rejectPhoto = async (photo_id: string) => {
-  try {
-    const response = await instance(`/photo/${photo_id}/reject`, {
-      method: 'DELETE',
-    })
-    revalidatePath('/post')
-    return response // 200 상태 코드는 fetchWithAuth에서 처리됨
-  } catch (error) {
-    // Handle error here
-    console.error('Error rejecting photo:', error)
-    throw error
-  }
+  const response = await instance(`/photo/${photo_id}/reject`, {
+    method: 'DELETE',
+  })
+  revalidatePath('/post')
+  return response // 200 상태 코드는 fetchWithAuth에서 처리됨
 }
